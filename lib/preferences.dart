eed90162d6b1241f9c45840ef79acbd705ea5c63
yaliza <123yak12@gmail.com>
2019-07-05 19:28:40 +0300

    Improve settings screen

--- lib/preferences.dart
+++ lib/preferences.dart
@@ -11,6 +11,21 @@ class PreferencesPage extends StatefulWidget {
 }
 
 class _PreferencesPageState extends State<PreferencesPage> {
+  String tempUnitValue = 'metric';
+  var tempUnits = ['metric', 'imperial'];
+  final appIdTextController = TextEditingController();
+
+  _PreferencesPageState() {
+    PreferencesHelper.getAppId().then((val) => changeAppId(val));
+    PreferencesHelper.getTempUnit().then((val) => changeTempUnit(val));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    appIdTextController.addListener(_onAppIdChanged);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -18,19 +33,62 @@ class _PreferencesPageState extends State<PreferencesPage> {
           title: new Text("Preferences"),
         ),
         body: Container(
-        child: DropdownButton<String>(
-          items: <String>['metric', 'imperial'].map((String value) {
-            return new DropdownMenuItem<String>(
-              value: value,
-              child: new Text(value),
-            );
-          }).toList(),
-          onChanged: (val) => _onTempUnitChanged(val),
-        ))
-    );
+            child: new Column(children: [
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              new Expanded(
+                child: Text('Temp unit: '),
+                flex: 1,
+              ),
+              new Expanded(
+                child: DropdownButton<String>(
+                    value: tempUnitValue,
+                    items: tempUnits.map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
+                        child: new Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (val) => _onTempUnitChanged(val)),
+                flex: 3,
+              )
+            ],
+          ),
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              new Expanded(
+                child: Text('Api key: '),
+                flex: 1,
+              ),
+              new Expanded(
+                child: TextField(
+                  controller: appIdTextController,
+                ),
+                flex: 3,
+              )
+            ],
+          )
+        ])));
+  }
+
+  void changeTempUnit(String value) {
+    setState(() {
+      tempUnitValue = value;
+    });
+  }
+
+  void changeAppId(String value) {
+    appIdTextController.text = value;
+  }
+
+  void _onAppIdChanged() {
+    PreferencesHelper.setAppId(appIdTextController.text);
   }
 
   void _onTempUnitChanged(String value) {
+    changeTempUnit(value);
     PreferencesHelper.setTempUnit(value);
   }
 }
