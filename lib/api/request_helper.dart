63bf9862d189df82e44cce28d4af0504e3b435ae
janawr <jana.wr.9@gmail.com>
2019-07-08 00:19:18 +0300

    Add background image, change weather data into card

--- lib/api/request_helper.dart
+++ lib/api/request_helper.dart
@@ -1,21 +1,47 @@
+import 'package:flutter_app/entities/weather_data.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_app/preferences_helper.dart';
 import 'package:flutter_app/entities/weather_info_prediction.dart';
 import 'dart:convert';
 
 class RequestHelper {
-
-  static void getPredictions(Function(List<WeatherInfoPrediction>) callback, Function error) async {
+  static void getPredictions(
+      Function(List<WeatherInfoPrediction>) callback, Function error) async {
     String link = 'http://api.openweathermap.org/data/2.5/forecast?'
-        'q=' + await  PreferencesHelper.getCity() +
-        '&units=' + await PreferencesHelper.getTempUnit() +
-        '&appid=' + await PreferencesHelper.getAppId();
+            'q=' +
+        await PreferencesHelper.getCity() +
+        '&units=' +
+        await PreferencesHelper.getTempUnit() +
+        '&appid=' +
+        await PreferencesHelper.getAppId();
 
     final response = await http.get(link);
 
     if (response.statusCode == 200) {
-      var weatherInfoPredictions = (json.decode(response.body)['list'] as List).map((i) => WeatherInfoPrediction.fromJson(i)).toList();
+      var weatherInfoPredictions = (json.decode(response.body)['list'] as List)
+          .map((i) => WeatherInfoPrediction.fromJson(i))
+          .toList();
       callback(weatherInfoPredictions);
+    } else {
+      error();
+    }
+  }
+
+  static void getCurrentWeather(
+      Function(WeatherData) callback, Function error) async {
+    String link = 'https://api.openweathermap.org/data/2.5/weather?'
+            'q=' +
+        await PreferencesHelper.getCity() +
+        '&units=' +
+        await PreferencesHelper.getTempUnit() +
+        '&appid=' +
+        await PreferencesHelper.getAppId();
+
+    final response = await http.get(link);
+
+    if (response.statusCode == 200) {
+      WeatherData data = WeatherData.fromJson(json.decode(response.body));
+      callback(data);
     } else {
       error();
     }
